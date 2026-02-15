b373ed55e7b5a50b9a6617d9a5689d25b330267f
anthonyeng <YOUR_EMAIL_THAT_IS ON the anthonyeng account>
2026-02-16 00:10:07 +0200

    fix axios api base

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,22 +1,9 @@
+// src/lib/api.js
 import axios from "axios";
-import { getToken, clearToken } from "./auth";
+
+export const API_BASE =
+  import.meta.env.VITE_API_BASE || "http://localhost:4000/api";
 
 export const api = axios.create({
-baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000",
+  baseURL: API_BASE,
 });
-
-api.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-api.interceptors.response.use(
-  (r) => r,
-  (err) => {
-    if (err?.response?.status === 401) clearToken();
-    return Promise.reject(err);
-  }
-);
-console.log("API BASE", import.meta.env.VITE_API_BASE_URL);
-
